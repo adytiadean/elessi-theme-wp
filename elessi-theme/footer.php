f49ae0cb18968cbae4a6a2873de2e80213d728d5
Adytia Dean Perdana <sentramaya.global@gmail.com>
2021-01-23 01:45:42 +0700

    initial setup

---
+++ elessi-theme/footer.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package nasatheme
+ */
+wp_footer();
+echo '</body></html>';
---
+++ elessi-theme/footers/footer-main.php
@@ -0,0 +1,7 @@
+    </div>
+    <!-- MAIN FOOTER -->
+    <footer id="nasa-footer" class="footer-wrapper nasa-clear-both">
+        <?php do_action('nasa_footer_layout_style'); ?>
+    </footer>
+    <!-- END MAIN FOOTER -->
+</div>
